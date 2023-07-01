99e5844175782e6944016bc750d3650c4af4879c
NSR270495 <102189181+NSR270495@users.noreply.github.com>
2023-07-01 12:47:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,26 @@ def hello():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True,host='0.0.0.0',port=port)
+
+#from flask import Flask, render_template
+
+#app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
